3a26a406e8730064d1ff1337e491fcc3a7b59238
Mankind71 <45287738+Mankind71@users.noreply.github.com>
2019-09-22 03:20:33 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>process form</title>
+	</head>
+
+	<body>
+	<?php
+	  $names = $_POST["name"];
+	  echo "Hello, " . $names . 	". Welcome To Team45";
+	?>
+	</body>
+
+</html>
---
+++ signin.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>signin form</title>
+	</head>
+
+	<body>
+	<?php
+	  $newnames = $_POST["username"];
+	  echo "Hello, " . $newnames . ". Welcome To Team45's Page";
+	?>
+	</body>
+
+</html>
